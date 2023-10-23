315706d2c7d9372db41a6b91839670964584c43f
nombre <211102@ids.upchiapas.edu.mx>
2023-10-22 21:49:29 -0600

    Sirve probablemte la conexcion a la base de datos, creo

--- src/database/mariaDb.ts
+++ src/database/mariaDb.ts
@@ -7,26 +7,24 @@ dotenv.config();
 const signale = new Signale();
 
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  database: process.env.DB_DATABASE || "soa-offi-cate",
+  password: process.env.DB_PASSWORD || "Miller2001Lopez",
+  port: 3308, // Agrega el puerto correspondiente aquí
   connectionLimit: 10,
 });
 
 export async function query(sql: string, params: any[]) {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    signale.success("Conexión exitosa a la BD");
-    const result = await conn.query(sql, params);
-    return result;
-  } catch (error) {
-    signale.error(error);
-    return null;
-  } finally {
-    if (conn) {
-      conn.release(); // Devuelve la conexión al pool al finalizar
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query(sql, params);
+        return rows;
+    } catch (error) {
+        signale.error(error);
+        return null;
+    } finally {
+        if (conn) conn.end();
     }
-  }0
 }
