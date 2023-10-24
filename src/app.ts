d80b41cd943c8eda53e91a3860d9c9e5835a468b
203467 <77993108+203467-ids@users.noreply.github.com>
2023-10-24 12:33:59 -0600

    corrigiendo errores de push

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,27 @@
 import express from 'express';
 import { Signale } from 'signale';
-<<<<<<< HEAD
+import "dotenv/config";
 import { personRoutes } from './person/infrastructure/personRouter';
 import { emailRouter } from './person/infrastructure/services/router/emailRouter';
+import { userRouter } from './user/infrastructure/userRouter'; 
 
 
-import "dotenv/config";
-import { userRouter } from './user/infrastructure/userRouter'; // Cambiado a userRouter
 
-
-=======
-import "dotenv/config";
-
-import { personRoutes } from './person/infrastructure/personRouter';
->>>>>>> 4937744aa77bfc5f0fd945fc037deae94137f9d1
 
 const app = express();
 const signale = new Signale();
 
 app.use(express.json());
-<<<<<<< HEAD
 
 
 app.use('/api/v1/person', personRoutes);
 app.use('/api/v1/person/email', emailRouter);
 app.use('/api/v1/user', userRouter); // Cambiado a userRouter
 
-=======
-app.use('/api/v1/person', personRoutes);
->>>>>>> 4937744aa77bfc5f0fd945fc037deae94137f9d1
 
 
 app.listen(3000, () => {
     signale.success("Server online in port 3000");
-<<<<<<< HEAD
+
 });
-=======
-});
->>>>>>> 4937744aa77bfc5f0fd945fc037deae94137f9d1
+
