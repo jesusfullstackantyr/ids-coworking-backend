701448fdcd3d054a06eb92e85283c64ea85eb821
Da-riot <193251@ids.upchiapas.edu.mx>
2023-11-06 11:39:31 -0600

    routes

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,19 @@ import express from 'express';
 import { Signale } from 'signale';
 import dotenv from 'dotenv';
 
-import { officeRouter } from './Office/infraestructure/officeRouter';
-import { categoryRoutes } from './Category/infraestructure/categoryRouter';
-import { ContractRoutes } from './contract/infrastructure/contractRouter';
+import { ContractRoutes } from './invoiceManagement/contract/infraestructure/routes/contractRouter';
 import { PaypalRoute } from './NewPaypal/infrestructure/paypalRouter';
+
+
+import { officeRouter } from './officeManagement/office/infraestructure/routes/officeRouter';
+import { categoryRoutes } from './officeManagement/category/infraestructure/routes/categoryRouter';
 import { personRoutes } from './person/infrastructure/personRouter';
 import { emailRouter } from './person/infrastructure/services/router/emailRouter';
-import { userRouter } from './user/infrastructure/userRouter'; 
-import { paymentRouter } from './payments/infraestrucuture/routers/paymentRouter';
-import { paymentsRouter } from './paymentMethod/infraestructure/routes/paymentMethodRouter';
+import { userRouter } from './user/infrastructure/userRouter';
+import { paymentRouter } from './invoiceManagement/payments/infraestructure/routers/paymentRouter';
+import { paymentsRouter } from './invoiceManagement/paymentMethod/infraestructure/routes/paymentMethodRouter';
+
+
 
 dotenv.config();
 
@@ -18,22 +22,23 @@ const app = express();
 const signale = new Signale();
 app.use(express.json());
 
-app.use('/contract',ContractRoutes);
-app.use("/Paypal", PaypalRoute);
-app.use('/payments', paymentRouter)
-app.use('/api/v1//Payment', paymentRouter);
-app.use('/api/v1/paymentsMethod',paymentsRouter);
+app.use('/api/v1/contracts', ContractRoutes);
+
+//app.use('/payments', paymentRouter)
+//app.use('/api/v1/Payment', paymentRouter);
+app.use('/api/v1/Payment', paymentRouter);
+
+app.use('/api/v1/payments_method',paymentsRouter);
+
 app.use('/api/v1/office', officeRouter);
-app.use('/api/v1/category', categoryRoutes);
+app.use('/api/v1/categories', categoryRoutes);
 app.use("/api/v1/cards", paymentsRouter)
 app.use('/api/v1/person', personRoutes);
 app.use('/api/v1/person/email', emailRouter);
 app.use('/api/v1/user', userRouter);
 
-
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 app.listen(SERVER_PORT, () => {
-    signale.success("Server online in port 3001");
+    signale.success(`API Gateway en el puerto ${SERVER_PORT}`);
 });
-
