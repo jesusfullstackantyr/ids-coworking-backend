ba3fb56bd81f2d1c61aaadf1e87b49f7cef858fa
203459 <203459@ids.upchiapas.edu.mx>
2023-11-05 23:01:15 -0600

    add-Docker-Compose

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Proyecto de SOA (Arquitectura Orientada a Servicios)
 
+# docker build -t server .
+
+# docker-compose up
+
+# docker build -t server .
+
 Este es el repositorio principal para el proyecto de SOA
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
 import express from 'express';
 import { Signale } from 'signale';
-import "dotenv/config";
+//import "dotenv/config";
 import { userRouter } from './accessManagement/user/infrastructure/routes/userRouter';
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const appid = process.env.APPID;
+var PORT = process.env.SERVER_PORT;
 const app = express();
 const signale = new Signale();
 
 app.use(express.json());
+//app.use(express.json());
+app.get("/", (req,res) => 
+res.send(`appid: ${appid} home page: says hello!`))
 app.use('/api/v1/user', userRouter);
 
-app.listen(3000, () => {
-    signale.success("Server online in port 3000");
+
+
+app.listen(PORT, () => {
+    signale.success(`Server run in port ${PORT}`);
 });
