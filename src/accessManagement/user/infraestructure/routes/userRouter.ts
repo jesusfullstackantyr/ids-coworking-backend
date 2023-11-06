6efadd99e201efa0b21ea8de3dcbabac6454be9e
Luis Daniel Molina <201252@ids.upchiapas.edu.mx>
2023-11-06 11:17:12 -0600

    correccion endpoints

--- src/accessManagement/user/infraestructure/routes/userRouter.ts
+++ src/accessManagement/user/infraestructure/routes/userRouter.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { getUserClientController } from '../dependencies';
 
-
 export const userRouter = express.Router();
 
 userRouter.get('/getUser/client/:id/:idRole', getUserClientController.run.bind(getUserClientController));
