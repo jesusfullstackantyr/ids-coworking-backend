c8475042eafb7b90efbf5e925b6e135ccafce8fa
203467 <77993108+203467-ids@users.noreply.github.com>
2023-11-05 21:42:52 -0600

    agregando dockerizado

---
+++ src/accessManagement/person/infrastructure/dependencies.ts
@@ -0,0 +1,15 @@
+import { MariadbPersonRepository } from "./mariadbPersonRepository";
+const mariadbPersonRepository= new MariadbPersonRepository();
+
+
+
+import { RegisterPersonController } from "./controllers/registerPersonController";
+import { RegisterPersonUseCase } from "../application/registerPersonUseCase";
+
+const registerPersonUseCase = new RegisterPersonUseCase(mariadbPersonRepository);
+const registerPersonController = new RegisterPersonController(registerPersonUseCase);
+
+export {
+    registerPersonController,
+    
+};
---
+++ src/database/mariaDb.ts
@@ -0,0 +1,32 @@
+import * as dotenv from "dotenv";
+import mariadb from "mariadb";
+import { Signale } from "signale";
+
+dotenv.config();
+
+const signale = new Signale();
+
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  connectionLimit: 10,
+});
+
+export async function query(sql: string, params: any[]) {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    signale.success("Conexión exitosa a la BD");
+    const result = await conn.query(sql, params);
+    return result;
+  } catch (error) {
+    signale.error(error);
+    return null;
+  } finally {
+    if (conn) {
+      conn.release(); // Devuelve la conexión al pool al finalizar
+    }
+  }
+}
